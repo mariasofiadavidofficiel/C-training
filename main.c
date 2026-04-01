4b9cdd2cae977259eb08240991a4de9a161f13e9
mariasofiadavidofficiel <maria.sofia.david.officiel@gmail.com>
2026-04-01 02:11:10 +0200

    Refine comments in main.c
    Updated comments to clarify the purpose of the code.

--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 /******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+  given int acsii codes 72, 101, 108, 108, 111, print the characters.
 
 *******************************************************************************/
 
