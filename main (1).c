916ff62c903912f2e8d73700c75d1ab32bf87d16
mariasofiadavidofficiel <maria.sofia.david.officiel@gmail.com>
2026-04-01 02:10:25 +0200

    Clean up comments and formatting in main.c
    Removed unnecessary comments and fixed formatting.

--- main (1).c
+++ main (1).c
@@ -1,12 +1,8 @@
 /******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+   given int acsii codes 72, 101, 108, 108, 111, print the characters.
 
 *******************************************************************************/
-
-#include <stdio.h>
 
 #include <stdio.h>
 
